34c203ee03a993c3e8bcbbe3a61531c04febb4a1
James Thomson <james_thomson@ntlworld.com>
2014-08-03 22:33:33 +0100

    adding d3 output

--- R/SpotifyAPI.R
+++ R/SpotifyAPI.R
@@ -76,7 +76,7 @@ getRelatedArtists<-function(getArtistsOutput) {
         temp<-data.frame(
           from_artist=getArtistsOutput[i,1],
           from_artist_id=getArtistsOutput[i,2], 
-          position=i,
+          position=j,
           to_artist=related$artists[[j]]$name,
           to_artist_id=substring(related$artists[[j]]$uri, as.integer(gregexpr(":", related$artists[[j]]$uri)[[1]])[2]+1, nchar(related$artists[[j]]$uri)),
           to_artist_pop=related$artists[[j]]$popularity
@@ -102,7 +102,7 @@ getRelatedArtists<-function(getArtistsOutput) {
         temp<-data.frame(
           from_artist=getArtistsOutput[i,1],
           from_artist_id=getArtistsOutput[i,2], 
-          position=i,
+          position=j,
           to_artist=related$artists[[j]]$name,
           to_artist_id=substring(related$artists[[j]]$uri, as.integer(gregexpr(":", related$artists[[j]]$uri)[[1]])[2]+1, nchar(related$artists[[j]]$uri)),
           to_artist_pop=related$artists[[j]]$popularity
@@ -143,9 +143,12 @@ visRelatedArtists<-function(artist, steps=2, output_file) {
     artists_relate<-getRelatedArtists(artists)
     artists_relate$step<-1
     
-    #update the artist lists
+    #update the artist list
     artist_list<-artists_relate[,c(4:6)]
-    artist_list_new<-artist_list[!(artist_list$to_artist %in% artists$artist),]  
+    #need to create a unique list
+    unique_artist_list<-aggregate(artist_list$to_artist_pop, by=list(artist_list$to_artist, artist_list$to_artist_id), FUN=max)
+    #find which ones are new
+    artist_list_new<-unique_artist_list[!(unique_artist_list$Group.1 %in% artists$artist),]  
     colnames(artist_list_new)<-c("artist", "artist_id", "artist_pop")
     artist_list_new$step<-1
     artists<-rbind(artists, artist_list_new) 
@@ -194,9 +197,190 @@ visRelatedArtists<-function(artist, steps=2, output_file) {
     
   }
   
+  nodes<-data.frame(name=artists$artist, group=artists$step, popularity=artists$artist_pop)
+  links<-data.frame(source=artists_relate$from_artist, target=artists_relate$to_artist, value=1, distance=40+artists_relate$position*5)
+
   
-  return(list(artists=artists, relationships=artists_relate))
+  
+  
+  
+  jsonConvert<-function(nodes, links){
+    
+    nodes<-data.frame(lapply(nodes, as.character), stringsAsFactors=FALSE)
+    #links<-data.frame(lapply(links, as.character), stringsAsFactors=FALSE)
+    links.lu<-NULL
+    
+    i<-2
+    for (i in 1:nrow(links)){
+    
+    temp<-data.frame(  
+      s=which(nodes$name==links$source[i])-1,
+      t=which(nodes$name==links$target[i])-1,
+      v=links$value[i],
+      d=links$distance[i])
+      
+      links.lu<-rbind(links.lu, temp)
+      
+    }
+    links.df<-data.frame(links.lu)
+    names(links.df)<-c("source", "target", "value", "distance")
+    
+    n<-dfToJSON(nodes,  'rowToObject')
+    e<-dfToJSON(links.df,  'rowToObject')
+    
+    json<-paste0("{ \"nodes\":", n, ", \"links\": ", e, "}")
+    return(list(Type="json:nodes_links", json=json))
+    
+  }
+  
+  Force<-function(JSON, file_out){
+    
+    if (JSON$Type!="json:nodes_links"){stop("Incorrect json type for this D3")}
+    
+    header<-"<!DOCTYPE html>
+    
+    <style>
+    
+    .link {
+    stroke: #ccc;
+    }
+    
+    .node text {
+    pointer-events: none;
+    font: 10px sans-serif;
+    }
+    
+    
+    </style>
+    <body>
+    <script src=\"http://d3js.org/d3.v3.min.js\"></script>
+    
+    <script type='text/javascript' src=\"http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js\"> </script>
+    
+    <script type=\"application/json\" id=\"mis\">"
+    
+    footer<-"</script>
+    
+    
+    
+    
+    <script>
+    //Constants for the SVG
+    var width = 1000,
+    height = 1000;
+    
+    //Set up the colour scale
+    var color = d3.scale.category20();
+    
+    //Set up the force layout
+    var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(function(d) { return  d.distance; }) 
+    .size([width, height]);
+    
+    //Append a SVG to the body of the html page. Assign this SVG as an object to svg
+    var svg = d3.select(\"body\").append(\"svg\")
+    .attr(\"width\", width)
+    .attr(\"height\", height);
+    
+    //Read the data from the mis element 
+    var mis = document.getElementById('mis').innerHTML;
+    graph = JSON.parse(mis);
+    
+    //Creates the graph data structure out of the json data
+    force.nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+    
+    //Create all the line svgs but without locations yet
+    var link = svg.selectAll(\".link\")
+    .data(graph.links)
+    .enter().append(\"line\")
+    .attr(\"class\", \"link\")
+    .style(\"stroke-width\", function (d) {return (d.value*0.5); });
+    
+    //Do the same with the circles for the nodes - no 
+    //Changed
+    var node = svg.selectAll(\".node\")
+    .data(graph.nodes)
+    .enter().append(\"g\")
+    .attr(\"class\", \"node\")
+    .call(force.drag);
+    
+    node.append(\"circle\")
+    .attr(\"r\", 8)
+    .attr(\"r\", function(d) { return d.popularity/5; })
+    .style(\"fill\", function (d) { return color(d.group)
+
+;
+})
+    
+    node.append(\"text\")
+    .attr(\"dx\", 10)
+    .attr(\"dy\", \".35em\")
+    .text(function(d) { return d.name });
+    //End changed
+    
+    
+   //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+    force.on(\"tick\", function () {
+    link.attr(\"x1\", function (d) {
+    return d.source.x;
+  })
+    .attr(\"y1\", function (d) {
+    return d.source.y;
+    })
+    .attr(\"x2\", function (d) {
+    return d.target.x;
+    })
+    .attr(\"y2\", function (d) {
+    return d.target.y;
+    });
+    
+    //Changed
+    
+    d3.selectAll(\"circle\").attr(\"cx\", function (d) {
+    return d.x;
+    })
+    .attr(\"cy\", function (d) {
+    return d.y;
+    });
+    
+    d3.selectAll(\"text\").attr(\"x\", function (d) {
+    return d.x;
+    })
+    .attr(\"y\", function (d) {
+    return d.y;
+    });
+    
+    //End Changed
+    
+});
+ </script>
+ </body>
+ </html>"
+    
+    fileConn<-file(file_out)
+    writeLines(paste0(header, JSON$json, footer), fileConn)
+    close(fileConn)
+    
+}     
+  
+    
+  
+  
+  json<-jsonConvert(nodes,links)
+  Force(json, file_out=output_file)
+
+  
+  
+  
+  
+  
+  
+  return(list(artists=artists, relationships=artists_relate, json=json))
   
 }
+
 
 
